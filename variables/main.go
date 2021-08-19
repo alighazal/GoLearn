4d207b0d0372129c75bf08e7d3d38b5075a40820
alighazal <ali.mdghazal@gmail.com>
2021-08-19 21:54:56 +0200

    learning variables and data types

---
+++ variables/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var num int = 5
+	var f float64 = 5.5
+	var str string = "sdsafasf"
+	const num_const int = 155
+
+	fmt.Println("Hello")
+	fmt.Println(num)
+	fmt.Println(f)
+	fmt.Println(str)
+	fmt.Println(num_const)
+}
